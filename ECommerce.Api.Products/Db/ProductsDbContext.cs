f74c220793573d9091d2112400fc5d2a5dd34d84
Robert Hardy <helloarchy@gmail.com>
2021-10-27 15:50:50 +0100

    Add a db context for Products.

--- ECommerce.Api.Products/Db/ProductsDbContext.cs
+++ ECommerce.Api.Products/Db/ProductsDbContext.cs
@@ -4,6 +4,11 @@ namespace ECommerce.Api.Products.Db
 {
     public class ProductsDbContext : DbContext
     {
-        
+        public DbSet<Product> Products { get; set; }
+
+        public ProductsDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
     }
 }
